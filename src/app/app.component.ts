eb1749433a631e7d14ea4daee43aa024e8265c07
Szymon Janecki <szjanecki@gmail.com>
2022-11-28 18:22:08 +0100

    feat: start working on todo item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './app.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todoFrontendMentor';
+
+  public items: TodoItem[] = [
+    { id: 1, title: 'item 1', isCompleted: false },
+    { id: 2, title: 'item 2', isCompleted: false },
+    { id: 3, title: 'item 3', isCompleted: true },
+  ];
 }
